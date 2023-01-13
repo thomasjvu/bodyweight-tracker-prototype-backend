013e1418cb43b030a2aed5411d4ace20e94af957
Thomas J. Vu <thomasjvu@gmail.com>
2023-01-13 02:30:06 -0500

    Add null validation to update controller

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -78,6 +78,23 @@ const deleteTracker = async (req, res) => {
 const updateTracker = async (req, res) => {
     const { id } = req.params
 
+    // Check for Empty Fields
+    let emptyFields = []
+    if (!weight) {
+        emptyFields.push('weight')
+    }
+    if (!date) {
+        emptyFields.push('date')
+    }
+
+    if (weight < 0 || weight > 1000) {
+        return res.status(400).json({error: 'Please make weight between 0 and 1000', emptyFields})
+    }
+
+    if (emptyFields.length > 0) {
+        return res.status(400).json({error: 'Please check all the fields', emptyFields})
+    }
+
     // check if ID is not valid
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({error: 'No such bodyweight tracker' })
